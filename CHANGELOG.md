d7f709f773ba9b1d7af0511c45fcda8b2e93b2b0
bigbyrus <86277019+bigbyrus@users.noreply.github.com>
2024-06-04 19:57:45 -0700

    Add files via upload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
-You know the drill...
+1. We added a skeleton of how our website will function, with different pages to be served on the raspberryPi, also tested the face recognition on its own to assure it works.
+
+
--- main.py
+++ main.py
@@ -1,264 +1,94 @@
 import cv2
-#import pytesseract
-from flask import Flask, render_template, request, Response, redirect, url_for, json, jsonify
+import pytesseract
+from flask import Flask, render_template, request, Response, redirect, url_for
 from PIL import Image
 from io import BytesIO
 import numpy as np
 import os
 import face_recognition
 from PIL import Image
-from datetime import datetime
 from io import BytesIO
 import base64
 import string
-import serial
-import struct
-import time
-import threading
-from inference_sdk import InferenceHTTPClient
-from inference import get_model
-import supervision as sv
-import roboflow
-import re
-import ast
-from urllib.parse import urlparse
 
-## TODO
-## 2. If no user_faces folder at runtime, make one
-## 3. Read JPG images into folder<username>
-## 4. Back buttons
-## 5. Clean code, make functions 
-## 6. rename variables, pages, and functions better
-## 7. Delete photo option
-## 7. Full storage 
-
-# Initialize Roboflow API with your API key
-#rf = roboflow.Roboflow(api_key="zxoKHEJwrJO7PNKlDol6Y4Bldpj1")
-#project = rf.workspace().project("PROJECT_ID")
-#model = project.version("1").model
-
-lastCaptureTime = 0
-captureInterval = 5
-
-## Scaling necessary for face_recognition, depends on esp vs webcam
-scale_up = 2
-scale_down = .5
-
-from inference_sdk import InferenceHTTPClient
-
-CLIENT = InferenceHTTPClient(
-    api_url="https://detect.roboflow.com",
-    api_key="3KBf2PlWJgxw65tAUXJA"
-)
-
-# Check for ESP32??? Correct Scaling
-try:
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=100)
-    scale_up = 2
-    scale_down = .5
-    #ser.close()
-except serial.SerialException as e:
-    print("Please check the port and try again.")
-
-# Need LeBron to poulate np array correctly
-honey_path = os.path.join(os.getcwd(), "tryAgain.jpg")
-honey_image = face_recognition.load_image_file(honey_path)
-lebron_path = os.path.join(os.getcwd(), "lebron.jpg")
-lebron_image = face_recognition.load_image_file(lebron_path)
-lebron_face_encoding = face_recognition.face_encodings(lebron_image)[0]
-app = Flask(__name__) 
-
-known_users = ["LBJ"]
-known_face_encodings = np.array([lebron_face_encoding])
-
-# Scans user_faces and reloads known_faces after reboot
-def load_faces_and_encodings(directory):
-    global known_users
-    global known_face_encodings
-    for file_name in os.listdir(directory):
-        # Check if the file is an image (you can add more extensions if needed)
-        if file_name.endswith(('.jpg', '.jpeg', '.png')):
-            image_path = os.path.join(directory, file_name)
-            image = face_recognition.load_image_file(image_path)
-
-            # Find the face locations and encodings in the imagew
-            face_encodings = face_recognition.face_encodings(image)
-
-            # Assuming there is one face per image, take the first encoding
-            if face_encodings:
-                known_face_encodings = np.append(known_face_encodings, [face_encodings[0]], axis=0)
-                known_users.append(os.path.splitext(file_name)[0])
-            else:
-                print(f"No faces found in image: {file_name}")
-
-user_directory = os.path.join(os.getcwd(),"static", "user_faces")
-load_faces_and_encodings(user_directory)
-
-stop_event = threading.Event()
-
-def listen_for_trigger():
-    global ser
-    while True:
-        try:
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').rstrip()
-                if line == "Take_Photo":
-                    #ser.reset_input_buffer()
-                    capture()
-                    
-        except Exception as e:
-            pass
-        time.sleep(0.1)  # Adjust the sleep time as needed
-    
-
-def start_listener():
-    global listener_thread
-    # Reset the stop event
-    stop_event.clear()
-    # Create a new thread instance and start it
-    listener_thread = threading.Thread(target=listen_for_trigger, daemon=True)
-    listener_thread.start()
-
-def stop_listener(listener_thread):
-    stop_event.set()
-    listener_thread.join()
-
+app = Flask(__name__)
+known_users = []
+known_face_encodings = []
 face_locations = []
 face_encodings = []
 face_names = []
+process_this_frame = True
+# Path to the Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-def read_image_from_serial(ser):
-    ser.write(b'TRIGGER')
-    ser.flushInput()
-    ser.flushOutput()
-    # Read the length of the image
-    img_len_bytes = ser.read(4)
-    img_len = int.from_bytes(img_len_bytes, 'little')
-    print(f"Image length: {img_len}")
+USER_DIR = os.path.join(os.getcwd(), "users")
 
-    # Read the image data
-    img_data = ser.read(img_len)
-    if len(img_data) != img_len:
-        print(f"Failed to read the full image. Read {len(img_data)} bytes.")
-        return None
+# Function to perform OCR on an image
+def ocr(image):
+    text = pytesseract.image_to_string(image)
+    return text if text.strip() else "no text found"
 
-    # Decode the image
-    img_array = np.frombuffer(img_data, dtype=np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    #//recognize_n_save(img)
-    ser.flushInput()
-    ser.flushOutput()
-    return img
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# get grayscale image
 def get_RGB(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def extract_prefix_before_number(url):
-    path = urlparse(url).path
-    image_name = path.split('/')[-1]
-    match = re.match(r'^[^\d]*', image_name)
-    if match:
-        return match.group()
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
     else:
-        return ""
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
 
-def reformat_image(image):
-    pil_image = Image.fromarray(image)
-    img_buffer = BytesIO()
-    pil_image.save(img_buffer, format="JPEG")
-    img_str = img_buffer.getvalue()
-    img_base64 = base64.b64encode(img_str).decode('utf-8')
-    return img_base64 
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
 
-def full_buffer():
-    global lastCaptureTime
-    currentTime = time.time()
-    if currentTime - lastCaptureTime < 3:
-        return True
-    lastCaptureTime = currentTime
-    return False
-
-def take_photo():
-    global ser
-    try:
-        image = read_image_from_serial(ser)
-    except Exception as e:
-        print("Please check the port and try again.(187)")
-    return image
-
-def recognize_n_save(image):
-    global ser
-    small_image = cv2.resize(image, (0, 0), fx=scale_down, fy=scale_down)
-    rgb_small_image = cv2.cvtColor(small_image, cv2.COLOR_BGR2RGB)
-    face_locations = face_recognition.face_locations(rgb_small_image)
-    new_face_encodings = face_recognition.face_encodings(rgb_small_image, face_locations)
-    face_names = []
-    for face_encoding in new_face_encodings:
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "???"
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            print("name found")
-            name = known_users[best_match_index]
-            now = datetime.now()
-            pil_image = Image.fromarray(image)
-            result = CLIENT.infer(image, model_id="ingredient-detection-5uzov/5") #Start Object Detection
-            res = str(result)
-            result_dict = ast.literal_eval(res) 
-            try:
-                pred_classes = result_dict["predictions"][0]
-                pred = pred_classes["class"]
-            except:
-                pred = "unknown"
-            word = str(pred) # End object Detection
-            print(word)
-            target_dir = os.path.join(user_directory, name, word+now.strftime("%Y-%m-%d-%H-%M-%S") + ".jpg")
-            pil_image.save(target_dir)
-            face_names.append(name)
-        else:
-            face_names.append("???")
-            
-    if(len(face_names) == 0): # Does the same thing but directs photos to"Unrecognized"
-        print("no faces")
-        now = datetime.now()
-        pil_image = Image.fromarray(image)
-        #NEW CODE TRIAL
-        result = CLIENT.infer(image, model_id="ingredient-detection-5uzov/5")
-        res = str(result)
-        #print(res)
-        result_dict = ast.literal_eval(res)
-        try:
-            pred_classes = result_dict["predictions"][0]
-            pred = pred_classes["class"]
-        except:
-            pred = "unknown"
-        word = str(pred) # End object Detection
-        print(word)
-        misc_dir = os.path.join(user_directory, "Unrecognized", word+now.strftime("%Y-%m-%d-%H-%M-%S") + ".jpg")
-        pil_image.save(misc_dir)
-
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the image we detected in was scaled to 1/4 size
-        top *= scale_up
-        right *= scale_up 
-        bottom *= scale_up
-        left *= scale_up
-        cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    return image  
-
+# Route for the home page
 @app.route('/')
 def index(): 
     return render_template('index.html')
 
 @app.route('/users')
 def users():
-    folders = get_folders(user_directory)
+    folders = get_folders(USER_DIR)
     return render_template('userPage.html', folders=folders)
 
 @app.route('/newUser')
@@ -267,33 +97,9 @@ def newUser():
 
 @app.route('/folder/<folder_name>')
 def folder(folder_name):
-    folder_path = os.path.join(user_directory, folder_name)
+    folder_path = os.path.join(USER_DIR, folder_name)
     contents = os.listdir(folder_path)
-    image_files = [f for f in contents if f.endswith(('.png', '.jpg', '.jpeg', '.gif'))]  # Filter only image files
-    image_data = []
-    for image_file in image_files:
-        additional_string = extract_prefix_before_number(image_file)
-        image_url = url_for('static', filename=os.path.join('user_faces', folder_name, image_file).replace('\\', '/'))
-        image_data.append({
-            'url': image_url,
-            'original_name': image_file,
-            'added_string': additional_string
-        })
-
-    return render_template('folder.html', folder_name=folder_name, image_data=image_data)
-
-@app.route('/delete', methods=['POST'])
-def delete_image():
-    data = request.get_json()
-    print(data)
-    image_url = data['image_url']
-    full_url = os.getcwd() + image_url
-    print(full_url)
-    try:
-        os.remove(full_url)
-        return jsonify({'status': 'success'}), 200
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+    return render_template('folder.html', folder_name=folder_name, contents=contents)
 
 def get_folders(directory):
     folders = []
@@ -309,44 +115,101 @@ def submit():
     known_users.append(username)
     image_bytes = base64.b64decode(image_data)
     image = Image.open(BytesIO(image_bytes))
-    save_path = os.path.join(user_directory, username + ".jpg")
+    save_path = os.path.join(os.getcwd(), "user_faces", username + ".png")
     image.save(save_path)
     this_image = face_recognition.load_image_file(save_path)
     this_face_encoding = face_recognition.face_encodings(this_image)
-    if this_face_encoding:
-        global known_face_encodings
-        known_face_encodings = np.vstack([known_face_encodings, this_face_encoding[0]])
-    else:
-        return "No face found in the image", 400
-    newUser_path = os.path.join(user_directory, username)
-    if not os.path.exists(newUser_path):
-        os.makedirs(newUser_path)
-        return f"Directory for username {username} created"
-    else:
-        return f"Directory for username {username} already exists"
+    known_face_encodings.append(this_face_encoding)
+    directory_path = os.path.join(os.getcwd(), "user_faces", username)
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    
+    return f"Directory for username {username} created"
+
 
 @app.route('/capture', methods=['POST'])
-def capture(): ## Triggered by physical and virtual button push
-    global honey_image
-    if(full_buffer()):
-        image = get_RGB(honey_image) ## Try again image
-        img_base64 = reformat_image(image) ## Convert to JPG, return as as bitstream
-        return {'text': '', 'image': img_base64}
-    image = take_photo() ## Get image from XIAO S3 Sense
-    image = get_RGB(image) ## Convert to RGB
-    recognized_image = recognize_n_save(image) ## Match face, draw box, save to user
-    img_base64 = reformat_image(recognized_image) ## Convert to JPG, return as as bitstream
-    return {'text': '', 'image': img_base64}
+def capture():
+    # Capture image from webcam
+    camera = cv2.VideoCapture(0)
+    return_value, image = camera.read()
+    camera.release()
+    clean = remove_noise(image)
+    gray = get_grayscale(image)
+    rgb = get_RGB(image)
+    image = get_RGB(image)
+    opened = opening(clean)
+    thresh = thresholding(gray)
+    cannied = canny(clean)
+    extracted_text = ocr(rgb)
+
+    pil_image = Image.fromarray(rgb)
+    img_buffer = BytesIO()
+    pil_image.save(img_buffer, format="JPEG")
+    img_str = img_buffer.getvalue()
+    import base64
+    img_base64 = base64.b64encode(img_str).decode('utf-8')
+    small_image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_image = cv2.cvtColor(small_image, cv2.COLOR_BGR2RGB)
+    face_locations = face_recognition.face_locations(rgb_small_image)
+    new_face_encodings = face_recognition.face_encodings(rgb_small_image, face_locations)
+
+    face_names = []
+    for face_encoding in new_face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = users[best_match_index]
+            face_names.append(name)
+        else:
+            face_names = ["Unknown"] * len(face_locations)
+
+        print(face_names)
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the image we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    pil_image = Image.fromarray(image)
+    # Convert image to base64 string
+    img_buffer = BytesIO()
+    pil_image.save(img_buffer, format="JPEG")
+    img_str = img_buffer.getvalue()
+    import base64
+    img_base64 = base64.b64encode(img_str).decode('utf-8')
+    return {'text': extracted_text, 'image': img_base64}
 
 @app.route('/captureNewUser', methods=['POST'])
 def newUserCapture():
-    # DO NOT REMOVE
-    image = take_photo()
-    image = get_RGB(image)
-    img_base64 = reformat_image(image)
-    return {'text': ' ', 'image': img_base64}
+    camera = cv2.VideoCapture(0)
+    return_value, image = camera.read()
+    camera.release()
+    clean = remove_noise(image)
+    gray = get_grayscale(image)
+    rgb = get_RGB(image)
+    opened = opening(clean)
+    thresh = thresholding(gray)
+    cannied = canny(clean)
+    extracted_text = ocr(gray)
+    pil_image = Image.fromarray(gray)
+    img_buffer = BytesIO()
+    pil_image.save(img_buffer, format="JPEG")
+    img_str = img_buffer.getvalue()
+    import base64
+    img_base64 = base64.b64encode(img_str).decode('utf-8')
+
+    return {'text': extracted_text, 'image': img_base64}
 
 if __name__ == '__main__':
-    start_listener()
-    app.run(host = "0.0.0.0", port=8000)
+    app.run(host = "0.0.0.0", port=8000, debug=True)
     ##python -m http.server 8000 --bind 0.0.0.0
